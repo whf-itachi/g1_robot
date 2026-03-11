a0828b5ae0e69e95408789b812922771a792c8b9
itachi <itachi_ashes@163.com>
2026-03-11 17:19:00 +0800

    change the db_path of face_node

--- src/g1_face/g1_face/face_node.py
+++ src/g1_face/g1_face/face_node.py
@@ -29,8 +29,7 @@ class FaceNode(Node):
         self.detector.prepare(ctx_id=0)
 
         # 使用包共享目录中的数据文件
-        package_share_dir = get_package_share_directory('g1_face')
-        db_path = os.path.join(package_share_dir, '../../data/face_database.json')
+        db_path = os.path.expanduser("~/haitch/g1_robot/data/face_database.json")
         # 如果包路径下没有，则尝试相对路径
         if not os.path.exists(db_path):
             db_path = os.path.join(
